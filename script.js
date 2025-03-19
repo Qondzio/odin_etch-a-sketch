2debfe043794cb6df866213dda5b68deaa34c35e
Qondzio <qondziolabaziewicz@gmail.com>
2025-03-19 22:56:02 +0100

    Added html, css and js file. For now there is a working function that allows creating specified amount of squares. Things need to be added: - random color on square hover - function to reset color of all squares to default

---
+++ script.js
@@ -0,0 +1,53 @@
+const selectButton=document.getElementById("selectButton");
+const resetButton=document.getElementById("resetButton");
+const container=document.querySelector(".container");
+let numberOfSquares;
+let horizontalDiv;
+
+
+
+selectButton.addEventListener("click", ()=>{
+    numberOfSquares=prompt("Enter the number of squares vertically and horizontally:");
+    createSquares();
+})
+
+
+resetButton.addEventListener("click", ()=>{for (let j = 0; j <numberOfSquares; j++) {
+           
+}
+})
+
+
+function getRandomColor()
+{
+    let r,g,b;
+
+    r=Math.floor(Math.random()*256);
+    g=Math.floor(Math.random()*256);
+    b=Math.floor(Math.random()*256);
+}
+
+
+function createSquares(){
+    container.innerHTML="";
+    
+    for (let i = 0; i < numberOfSquares; i++) {
+        createHorizontalDiv();
+
+        for (let j = 0; j <numberOfSquares; j++) {
+           createDiv();
+        }
+    }
+}
+
+function createHorizontalDiv(){
+    horizontalDiv=document.createElement("div");
+    horizontalDiv.classList.add("squareHorizontally");
+    container.appendChild(horizontalDiv);
+}
+
+function createDiv(){
+    let div=document.createElement("div");
+    div.classList.add("square");
+    horizontalDiv.appendChild(div);
+}
